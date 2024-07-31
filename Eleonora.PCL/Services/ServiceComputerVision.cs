bbcea13898703fa0e6c95b4fd147fbb567d58773
tertun <tertun@tutamail.com>
2024-07-31 23:28:45 +0300

    Improved UX

---
+++ Eleonora.PCL/Services/ServiceComputerVision.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.ProjectOxford.Vision;
+using Microsoft.ProjectOxford.Vision.Contract;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Eleonora.PCL.Services
+{
+    public class ServiceComputerVision
+    {
+        private string SubscriptionKey = "";
+        private string apiRoot = @"https://westeurope.api.cognitive.microsoft.com/vision/v1.0";
+        /// <summary>
+        /// Get a list of available domain models
+        /// </summary>
+        /// <returns></returns>
+        public async Task<ModelResult> GetAvailableDomainModels()
+        {
+            VisionServiceClient VisionServiceClient = new VisionServiceClient(SubscriptionKey, apiRoot);
+            //
+            // Analyze the url against the given domain
+            //
+            ModelResult modelResult = await VisionServiceClient.ListModelsAsync();
+            return modelResult;
+        }
+        public async Task<AnalysisInDomainResult> MakeAnalysisRequest(Stream stream, Model model)
+        {
+            // OcrResults text;
+            VisionServiceClient client = new VisionServiceClient(SubscriptionKey, apiRoot);
+            AnalysisInDomainResult analysisResult = await client.AnalyzeImageInDomainAsync(stream, model);
+
+            return analysisResult;
+        }
+        
+    }
+}
---
+++ Eleonora.PCL/Services/ServiceImage.cs
@@ -0,0 +1,29 @@
+﻿using Plugin.Media.Abstractions;
+using Plugin.Media;
+using System.Threading.Tasks;
+
+namespace Eleonora.PCL.Services
+{
+    public class ServiceImage
+    {
+        public static async Task<MediaFile> TakePicture(bool useCam = true)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (useCam)
+            {
+                if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+                {
+                    return null;
+                }
+            }
+            var file = useCam ? await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+            {
+                Directory = "Championship",
+                Name = "photo.jpg"
+            }) : await CrossMedia.Current.PickPhotoAsync();
+
+            return file;
+        }
+    }
+}
